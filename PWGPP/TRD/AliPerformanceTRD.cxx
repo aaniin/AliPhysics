65f8bf97a3a1fc4d4b6d7f790cf49dd0cd7a55fb
syrkowski <syrkowski@physi.uni-heidelberg.de>
2017-04-21 15:48:43 +0200

    improved histograms

--- PWGPP/TRD/AliPerformanceTRD.cxx
+++ PWGPP/TRD/AliPerformanceTRD.cxx
@@ -2,11 +2,12 @@
 //Author: Sebastian Syrkowski, syrkowski@physi.uni-heidelberg.de
 
 #include "TH1F.h"
+#include "TH2F.h"
 #include "TChain.h"
 #include "TList.h"
 #include "AliESDEvent.h"
 #include "AliESDtrack.h"
-#include "AliESDTrdTrack.h"
+#include "AliESDTrdTracklet.h"
 
 #include "AliPerformanceTRD.h"
 #include "AliESDInputHandler.h"
@@ -14,8 +15,6 @@
 #include "AliAnalysisManager.h"
 
 #include "AliLog.h"
-//#include "iostream"
-//#include <AliSysInfo.h>
 
 //______________________________________________________________________________
 AliPerformanceTRD::AliPerformanceTRD():
@@ -29,11 +28,14 @@ AliPerformanceTRD::AliPerformanceTRD():
     fHistPt_trd(0),
     fHistNTracklets_trd(0),
     fHistPhi_trd(0),
-    fHistTRDSector(0),
-    fHistTRDStack(0),
+    fHistNTrdClustersPerTrack_trd(0),
+    fHistNTrdClustersPerTracklet_trd(0),
+    fHistTrdSclice_trd(0),
     fHistNTracks(0),
     fHistNTrdTracks(0),
-    fHistNTrdTracklets(0)
+    fHistNTrdTracklets_counted(0),
+    fHistNTrdTracklets(0),
+    fHistHalfChamberId(0)
 {
   //  ROOT  IO  constructor , don 't  allocate  memory  here!
 }
@@ -49,11 +51,14 @@ AliPerformanceTRD::AliPerformanceTRD(const  char* name):
     fHistPt_trd(0),
     fHistNTracklets_trd(0),
     fHistPhi_trd(0),
-    fHistTRDSector(0),
-    fHistTRDStack(0),
+    fHistNTrdClustersPerTrack_trd(0),
+    fHistNTrdClustersPerTracklet_trd(0),
+    fHistTrdSclice_trd(0),
     fHistNTracks(0),
     fHistNTrdTracks(0),
-    fHistNTrdTracklets(0)
+    fHistNTrdTracklets_counted(0),
+    fHistNTrdTracklets(0),
+    fHistHalfChamberId(0)
 {
     DefineInput (0, TChain::Class ());
     DefineOutput (1, TList::Class());
@@ -73,19 +78,23 @@ void AliPerformanceTRD::UserCreateOutputObjects()
     fOutputList -> SetOwner(kTRUE);
 
     //  create  our  histo  and  add  it  to  the  list
-    fHistPt_global = new TH1F("fHistPt_global", "transversal momentum, all tracks", 100, 0, 100);
+    fHistPt_global = new TH1F("fHistPt_global", "transversal momentum, all tracks", 40, 0, 20);
     fHistNTracklets_global = new TH1F("fHistNTracklets_global", "number of TRD tracklets per track, all tracks", 11, -0.5, 10.5);
-    fHistPhi_global = new TH1F("fHistPhi_global", "Phi (azimutal angle of momentum), all tracks", 100, 0, 10);
-
-    fHistPt_trd = new TH1F("fHistPt_trd", "transversal momentum, TRD tracks only", 100, 0, 100);
+    fHistPhi_global = new TH1F("fHistPhi_global", "Phi (azimutal angle of momentum), all tracks", 100, 0, 2*3.14159265358979323846);
+    
+    fHistPt_trd = new TH1F("fHistPt_trd", "transversal momentum, TRD tracks only", 40, 0, 20);
     fHistNTracklets_trd = new TH1F("fHistNTracklets_trd", "number of TRD tracklets per track, TRD tracks only", 11, -0.5, 10.5);
-    fHistPhi_trd = new TH1F("fHistPhi_trd", "Phi (azimutal angle of momentum), TRD tracks only", 100, 0, 10);
-    fHistTRDSector = new TH1F("fHistTRDSector", "TRD Sector of each TRD track", 21, -0.5, 20.5);
-    fHistTRDStack = new TH1F("fHistTRDStack", "TRD Stack of each TRD track", 11, -0.5, 10.5);
+    fHistPhi_trd = new TH1F("fHistPhi_trd", "Phi (azimutal angle of momentum), TRD tracks only", 100, 0, 2*3.14159265358979323846);
+    fHistNTrdClustersPerTrack_trd = new TH1F("fHistNTrdClustersPerTrack_trd", "number of TRD clusters per track, TRD tracks only", 200, 0, 200);
+    fHistNTrdClustersPerTracklet_trd = new TH1F("fHistNTrdClustersPerTracklet_trd", "number of TRD clusters per track divided by number of tracklets, TRD tracks only", 50, 0, 50);
+    fHistTrdSclice_trd = new TH2F("fHistTrdSclice_trd","slice 0 (total integrated charge) for each layer, TRD tracks only", 100, 0, 6000, 9, -1.5, 7.5);
 
-    fHistNTracks = new TH1F("fHistNTracks", "number of tracks per event", 100, 0, 100);
+    fHistNTracks = new TH1F("fHistNTracks", "number of tracks per event", 100, 0, 500);
     fHistNTrdTracks = new TH1F("fHistNTrdTracks","number of TRD tracks per event", 100, 0, 100);
-    fHistNTrdTracklets = new TH1F("fHistNTrdTracklets","number of TRD tracklets per event", 1000, 0, 1000);
+    fHistNTrdTracklets_counted = new TH1F("fHistNTrdTracklets_counted","number of counted TRD tracklets per event", 100, 0, 1000);
+    fHistNTrdTracklets = new TH1F("fHistNTrdTracklets","number of TRD tracklets per event", 100, 0, 1000);
+    
+    fHistHalfChamberId = new TH1F("fHistHalfChamberId", "TRD tracklet half-chamber ID", 200,0,200);
 
     // add histogggrams to our output list
     fOutputList -> Add(fHistPt_global);
@@ -95,12 +104,16 @@ void AliPerformanceTRD::UserCreateOutputObjects()
     fOutputList -> Add(fHistPt_trd);
     fOutputList -> Add(fHistNTracklets_trd);
     fOutputList -> Add(fHistPhi_trd);
-    fOutputList -> Add(fHistTRDSector);
-    fOutputList -> Add(fHistTRDStack);
+    fOutputList -> Add(fHistNTrdClustersPerTrack_trd);
+    fOutputList -> Add(fHistNTrdClustersPerTracklet_trd);
+    fOutputList -> Add(fHistTrdSclice_trd);
 
     fOutputList -> Add(fHistNTracks);
     fOutputList -> Add(fHistNTrdTracks);
+    fOutputList -> Add(fHistNTrdTracklets_counted);
     fOutputList -> Add(fHistNTrdTracklets);
+    
+    fOutputList -> Add(fHistHalfChamberId);
 
     //  add  the  list  to  our  output  file
     PostData(1, fOutputList);
@@ -136,36 +149,54 @@ void AliPerformanceTRD::UserExec(Option_t*)
         return;
       }
     }
-
+    
+    int nTracks=0;
+    int nTrdTracks=0;
+    int nTrdTracklets=0;
+    
     //loop over all tracks
-    const Int_t  nTracks(fESDEvent->GetNumberOfTracks());
-    for(Int_t iTrack(0); iTrack < nTracks; iTrack++)
+    const Int_t  nT(fESDEvent->GetNumberOfTracks());
+    for(Int_t iT(0); iT < nT; iT++)
     {
-      AliESDtrack *track = fESDEvent->GetTrack(iTrack);
+      AliESDtrack *track = fESDEvent->GetTrack(iT);
       if(!track) continue;
-      //  fill  our  histogram
+      nTracks++;
+      
+      //  fill histograms with all tracks
       fHistPt_global->Fill(track->Pt());
       fHistNTracklets_global->Fill(track->GetTRDntracklets());
       fHistPhi_global->Fill(track->Phi());
+      
+      // fill histograms with tracks containing TRD information
+      if (track->GetTRDntracklets()>0)
+      {
+        nTrdTracks++;
+        nTrdTracklets+=track->GetTRDntracklets();
+        
+        fHistPt_trd->Fill(track->Pt());
+        fHistNTracklets_trd->Fill(track->GetTRDntracklets());
+        fHistPhi_trd->Fill(track->Phi());
+        fHistNTrdClustersPerTrack_trd->Fill(track->GetNumberOfTRDClusters());
+        fHistNTrdClustersPerTracklet_trd->Fill(track->GetNumberOfTRDClusters()/((Float_t)track->GetTRDntracklets()));
+        
+        for (int iLayer(0); iLayer<track->kTRDnPlanes; iLayer++)
+        {
+          fHistTrdSclice_trd->Fill(track->GetTRDslice(iLayer,0), iLayer); //slice 0: integrated total charge
+        }
+      }
     }
-
-    //loop over TRD Tracks only
-    const Int_t  nTRDTracks(fESDEvent->GetNumberOfTrdTracks());
-    for(Int_t iTrack(0); iTrack < nTRDTracks; iTrack++)
+    
+    // fill tracklet based histograms
+    for (Int_t iTracklet(0); iTracklet<fESDEvent->GetNumberOfTrdTracklets(); iTracklet++)
     {
-      AliESDTrdTrack *trdtrack = fESDEvent->GetTrdTrack(iTrack);
-      if(!trdtrack) continue;
-      //  fill  our  histogram
-      fHistPt_trd->Fill(trdtrack->Pt());
-      fHistNTracklets_trd->Fill(trdtrack->GetNTracklets());
-      fHistPhi_trd->Fill(trdtrack->Phi());
-      fHistTRDSector->Fill(trdtrack->GetSector());
-      fHistTRDStack->Fill(trdtrack->GetStack());
+      AliESDTrdTracklet *tracklet =	fESDEvent->GetTrdTracklet(iTracklet);
+      fHistHalfChamberId->Fill(tracklet->GetHCId());
     }
-
+    
     // fill event based histograms
-    fHistNTracks->Fill(fESDEvent->GetNumberOfTracks());
-    fHistNTrdTracks->Fill(fESDEvent->GetNumberOfTrdTracks());
+    fHistNTracks->Fill(nTracks);
+    fHistNTrdTracks->Fill(nTrdTracks);
+    fHistNTrdTracklets_counted->Fill(nTrdTracklets);
     fHistNTrdTracklets->Fill(fESDEvent->GetNumberOfTrdTracklets());
 
     //  and  save  the  data  gathered  in  this  iteration
--- PWGPP/TRD/AliPerformanceTRD.h
+++ PWGPP/TRD/AliPerformanceTRD.h
@@ -4,6 +4,7 @@
 // forward declarations
 class TList;
 class TH1F;
+class TH2F;
 class AliESDEvent;
 
 #include "AliAnalysisTaskSE.h"  // ?
@@ -33,21 +34,26 @@ class  AliPerformanceTRD : public  AliAnalysisTaskSE
     Bool_t fUseHLT; //! use HLT
 
     //global track histograms
-    TH1F *fHistPt_global; //!  transversal momentum, global
+    TH1F *fHistPt_global; //! transversal momentum, global
     TH1F *fHistNTracklets_global; //! number of TRD tracklets per track, global
-    TH1F *fHistPhi_global; // azimutal angle of momentum, global
+    TH1F *fHistPhi_global; //! azimutal angle of momentum, global
 
     //TRD track histograms
     TH1F *fHistPt_trd; //!  transversal momentum, TRD tracks only
     TH1F *fHistNTracklets_trd; //! number of TRD tracklets per TRD track, TRD tracks only
-    TH1F *fHistPhi_trd; // azimutal angle of momentum, TRD tracks only
-    TH1F *fHistTRDSector; // TRD sector
-    TH1F *fHistTRDStack; // TRD stack
+    TH1F *fHistPhi_trd; //! azimutal angle of momentum, TRD tracks only
+    TH1F *fHistNTrdClustersPerTrack_trd; //! number of TRD clusters per track, TRD tracks only
+    TH1F *fHistNTrdClustersPerTracklet_trd; //! number of TRD clusters per tracklet, TRD tracks only
+    TH2F *fHistTrdSclice_trd; //! value of integrated TRD slices per track for each layer, TRD tracks only
 
     //event based histograms
     TH1F *fHistNTracks; //! number of tracks per event
     TH1F *fHistNTrdTracks; //! number of TRD tracks per event
+    TH1F *fHistNTrdTracklets_counted; //! number of counted TRD tracklets per event
     TH1F *fHistNTrdTracklets; //! number of TRD tracklets per event
+    
+    //tracklet based histograms
+    TH1F *fHistHalfChamberId; //! tracklet half-chamber ID
 
 
     //not implemented
